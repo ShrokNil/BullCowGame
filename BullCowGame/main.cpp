fa4c02bb17bea9e666968a18ec898217c1f9d687
ShrokNil <31967507+ShrokNil@users.noreply.github.com>
2018-07-20 19:35:06 -0600

    Refactored Code into more functional format

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+void PrintIntro();
+string Guess = "";
+string GetGuessAndPrintBack();
+
+
+// The entry point for our application
 int main()
 
 {
-	// << puts "Welcome to bulls and Cows\n" into std::cout
-	std::cout << "Welcome to bulls and Cows\n";
+	PrintIntro();
+	GetGuessAndPrintBack();
+	
+	
+
+
+
 	return 0;
+}
+
+void PrintIntro()
+{
+	// intoduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to bulls and Cows\n\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n\n";
+	cout << endl;
+	return;
+}
+
+// get a guess from the player
+string GetGuesAndPrintBack()
+{
+	
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	cout << endl;
+	// Prints the guess back
+	cout << "your guess was: " << Guess << endl;
+	cout << endl;
+	return Guess;
 }
